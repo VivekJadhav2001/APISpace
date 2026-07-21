1623b95ddf10a246896423d90eca09ab6ef6aa1f
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2026-07-21 22:24:04 +0530

    feat: add index.html for API documentation and landing page

--- src/server.js
+++ src/server.js
@@ -4,14 +4,20 @@ import connectDB from "./db/index.js"
 import { globalError, globalResponse } from "./middlewares/response.middleware.js"
 dotenv.config()
 import todoRouter from "./routes/apps/todo.routes.js"
+import path from "path";
+import { fileURLToPath } from "url";
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 connectDB()
 app.use(express.json())
 app.use(globalResponse)
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 
 app.use("/todos",todoRouter)
