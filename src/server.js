15a21ce31740ae7a6cd68d2b8212c04b6544f406
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2026-07-21 22:27:14 +0530

    fix: correct path to index.html in root route

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(globalResponse)
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 
